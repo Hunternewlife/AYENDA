105b9a5b17a9fc4b556381a5909c1d21a29d2962
David Rueda <hunternewlife@hotmail.com>
2020-05-28 17:00:41 -0500

    maquetacion base

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php session_start(); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AYENDA</title>
+    <script src="https://kit.fontawesome.com/7045981063.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="sources/styles/style.css">
+</head>
+<body>
+    <div class="banner">
+        <div class="titulo">
+            <a>AYENDA</a>
+            <div class="text">
+                <p>La mejor app</p>
+                <p>Para agendar tus clientes</p>
+            </div>
+        </div>
+        <div class="botones">
+            <div class="comercios"></div>
+            <div class="busqueda">
+                <input type="text" placeholder="buscar">
+                <h2><i class="fas fa-search"></i></h2>
+            </div>
+        </div>
+    </div>
+    <div class="negocios"></div>
+</body>
+</html>
